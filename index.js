71c3b85778b69b9331e11b50a0d5e2d492a0522f
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-09-05 20:31:44 +0300

    commit tree 48a79ccbd743fa407785025b87bef8bf6170c816

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindrome = word.split("").reverse().join("")
+  if (word === palindrome) {
+    return true;
+  } else{
+    return false;
+  }
 }
+ //isPalindrome("mama");
 
 /* 
   Add your pseudocode here
+
+  Declare a function
+  Set parameters of string
+  Declare a split to the string
+  reverse the split
+  Declare a join
+  input string;
+  if (string === string)
+    then
+    return true,
+  else
+    return false
+  output string
 */
 
 /*
   Add written explanation of your solution here
+
+  The code receives a string as a parameter in the function isPalindrome
+  . uses the split to make the string of individual characters, then reverses the
+  split with join. If the string input is equals to its palindrome then the return value is true
+  else the return value is false.
 */
 
 // You can run `node index.js` to view these console logs
